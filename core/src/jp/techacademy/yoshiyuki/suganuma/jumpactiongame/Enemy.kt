1abfcbab961e35d197eca63ee7368eed7793be98
sefeeld <sefeeld@yahoo.co.jp>
2020-08-30 21:55:56 +0900

    レッスン７課題ジャンプアクションゲーム

---
+++ core/src/jp/techacademy/yoshiyuki/suganuma/jumpactiongame/Enemy.kt
@@ -0,0 +1,39 @@
+package jp.techacademy.yoshiyuki.suganuma.jumpactiongame
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+
+class Enemy(texture: Texture, srcX: Int, srcY: Int, srcWidth: Int, srcHeight: Int)
+    : GameObject(texture, srcX, srcY, srcWidth, srcHeight) {
+
+    companion object {
+        // 横幅、高さ
+        val ENEMY_WIDTH = 0.8f
+        val ENEMY_HEIGHT = 0.8f
+
+        // 速度
+        var ENEMY_VELOCITY = 2.8f
+    }
+
+
+    init {
+        setSize(ENEMY_WIDTH, ENEMY_HEIGHT)
+        velocity.x = ENEMY_VELOCITY
+    }
+
+    // 座標を更新する
+    fun update(deltaTime: Float) {
+
+        x += velocity.x * deltaTime
+
+        if (x < Enemy.ENEMY_WIDTH / 2) {
+            velocity.x = -velocity.x
+            x = Enemy.ENEMY_WIDTH / 2
+        }
+        if (x > GameScreen.WORLD_WIDTH - Enemy.ENEMY_WIDTH / 2) {
+            velocity.x = -velocity.x
+            x = GameScreen.WORLD_WIDTH - Enemy.ENEMY_WIDTH / 2
+        }
+
+    }
+}
